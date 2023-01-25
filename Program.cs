fac06802236185c7477427617330718d73344268
Nastya <anastasiya.komova@mail.ru>
2023-01-25 17:39:58 +0300

    Добавлено решение задачи на языке  C#

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] arrayOne = new string[6] {"530", "083", "end", "world", "hello" , ".,k"};
+string[] arrayTwo = new string[arrayOne.Length];
+
+void newArrayString3(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if(arrayOne[i].Length < 4)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
+
+void outputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+Console.WriteLine();
+}
+
+newArrayString3(arrayOne, arrayTwo);
+outputArray(arrayTwo);
